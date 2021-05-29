2441e9d9e91312f51385a9be967d66029fad508e
Hana Stupková <hstupkova@gmail.com>
2021-05-29 10:27:19 +0200

    routování

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 export const App = () => (
-  <>
+  <Router>
     <Header />
-    <Home />
+
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/reservation">
+        <Reservation />
+      </Route>
+    </Switch>
+    
     <Footer />
-  </>
+  </Router>
 );
+
